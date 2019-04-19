2baf3b5135609e2233858c7737626127b9bf8633
sam990 <sam861g@gmail.com>
2019-04-18 20:21:00 -0700

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+    var quizArray = [{
+        question: "What City Are the Heat in?",
+        answer: ["Baltimore", "Paris", "Orlando", "Miami"],
+        correct: ["Miami"]
+    }, {
+        question: "How Many championships have the Boston Celtics won?",
+        answer: ["0", "18", "7", "21"],
+        correct: ["21"]
+    }, {
+        question: "Who does Steph Curry play for",
+        answer: ["Lakers", "Warriors", "Knicks", "Cleveland"],
+        correct: ["Warriors"]
+    }, {
+        question: "How many points did Kobe score in his last game?",
+        answer: ["24", "8", "45", "60"],
+        correct: ["60"]
+    },{
+        question: "Who won the NBA Championship in 2018?",
+        answer: ["Warriors", "Kobe", "Spurs", "Thunder"],
+        correct: ["Warriors"]
+    },
+    {
+        question: "How Many Teams Are in the NBA?",
+        answer: ["3", "25", "30", "33"],
+        correct: ["30"]
+    },{
+        question: "How Many Games does each team play at home?",
+        answer: ["50", "41", "20", "82"],
+        correct: ["41"]
+    },{
+        question: "How many feet high is Basketball rim?",
+        answer: ["10 feet", "25 feet", "20 feet", "1 feet"],
+        correct: ["10 feet"]
+    },{
+        question: "Which player has score 100 points in a game?",
+        answer: ["Wilt Chaimberlain", "Kobe Bryant", "Lebron James", "Kevin Durant"],
+        correct: ["Wilt Chaimberlain"]
+    },{
+        question: "What year was the Lakers last championship?",
+        answer: ["2003", "1993", "2010", "2005"],
+        correct: ["2010"]
+     }];
+
+     console.log(quizArray.question); 
+
+
+
+
+
+});
